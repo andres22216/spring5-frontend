5de00764e1e711b24f17f0161fdf094859e4fe42
andres22216 <and22216@hotmail.com>
2020-09-15 00:04:43 -0500

    Se deja el proyecto listo con todo el CRUD

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,16 @@ import { FooterComponent } from "./footer/footer.component";
 import { DirectivaComponent } from "./directiva/directiva.component";
 import { ClientesComponent } from "./clientes/clientes.component";
 import { RouterModule, Routes } from "@angular/router";
+import { HttpClientModule } from "@angular/common/http";
+import { FormComponent } from "./clientes/form.component";
+import { FormsModule } from "@angular/forms";
 
 const routes: Routes = [
   { path: "", redirectTo: "/clientes", pathMatch: "full" },
   { path: "directivas", component: DirectivaComponent },
   { path: "clientes", component: ClientesComponent },
+  { path: "clientes/form", component: FormComponent },
+  { path: "clientes/form/:id", component: FormComponent },
 ];
 
 @NgModule({
@@ -21,8 +26,14 @@ const routes: Routes = [
     FooterComponent,
     DirectivaComponent,
     ClientesComponent,
+    FormComponent,
   ],
-  imports: [BrowserModule, RouterModule.forRoot(routes)],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot(routes),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { Cliente } from "./cliente";
 import { ClienteService } from "./cliente.service";
+import swal from "sweetalert2";
 
 @Component({
   selector: "app-clientes",
@@ -16,5 +17,40 @@ export class ClientesComponent implements OnInit {
     this.clienteService
       .getClientes()
       .subscribe((clientes) => (this.clientes = clientes));
+  }
+
+  delete(cliente: Cliente): void {
+    const swalWithBootstrapButtons = swal.mixin({
+      customClass: {
+        confirmButton: "btn btn-success",
+        cancelButton: "btn btn-danger",
+      },
+      buttonsStyling: false,
+    });
+
+    swalWithBootstrapButtons
+      .fire({
+        title: "Está seguro?",
+        text: `¿Seguro que desea eliminar al cliente ${cliente.nombre}${" "}${
+          cliente.apellido
+        }?`,
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonText: "Si, eliminar!",
+        cancelButtonText: "No, cancelar!",
+        reverseButtons: true,
+      })
+      .then((result) => {
+        if (result.isConfirmed) {
+          this.clienteService.delete(cliente.id).subscribe((respuesta) => {
+            this.clientes = this.clientes.filter((cli) => cli !== cliente);
+            swalWithBootstrapButtons.fire(
+              "Eliminado!",
+              `El cliente ${cliente.nombre} ha sido eliminado.`,
+              "success"
+            );
+          });
+        }
+      });
   }
 }
